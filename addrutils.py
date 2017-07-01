bdb077d2d5cb8e6a2a01b8e6dc0c4ee1d085110c
Neil Zhao <zzrcxb@gmail.com>
2017-07-01 14:03:55 +0000

    Correct Python file into PEP8 standard

--- addrutils.py
+++ addrutils.py
@@ -1,38 +1,40 @@
-import	ast
-import	sys
-from	triton	import *
+import ast
+import sys
+from triton import *
+
 
 class ElfAddrs:
 
-    def __init__(self,filename):
+    def __init__(self, filename):
         self.binary = Elf(filename)
-	self.syms = self.binary.getSymbolsTable()
-	self.elfStart = 0
-	self.elfEnd = 0
+        self.syms = self.binary.getSymbolsTable()
+        self.elfStart = 0
+        self.elfEnd = 0
 
     def getSymAddr(self, symName, flag):
-	for sym in self.syms:
-	    if sym.getName() == symName:
+        for sym in self.syms:
+            if sym.getName() == symName:
                 addrSymStart = sym.getValue()
-	    	if sym.getSize() > 0:
-  	            addrSymEnd = addrSymStart + sym.getSize() - 1
-	    	else:
-  	            addrSymEnd = addrSymStart + sym.getSize()
-            	#print 'symbol %s_has been founded, addr: %x - %x' %(symName, addrSymStart, addrSymEnd)
-            	break
-    	if flag:
-       	    return addrSymEnd
-    	else:
-       	    return addrSymStart
+                if sym.getSize() > 0:
+                    addrSymEnd = addrSymStart + sym.getSize() - 1
+                else:
+                    addrSymEnd = addrSymStart + sym.getSize()
+                # print 'symbol %s_has been founded, addr: %x - %x' %(symName,
+                # addrSymStart, addrSymEnd)
+                break
+        if flag:
+            return addrSymEnd
+        else:
+            return addrSymStart
 
     def getSelfAddr(self):
         self.elfStart = self.getSymAddr('_init', 0)
-    	self.elfEnd = self.getSymAddr('_end', 0)
-    	return (self.elfStart,self.elfEnd)
+        self.elfEnd = self.getSymAddr('_end', 0)
+        return (self.elfStart, self.elfEnd)
 
     def isLocalInstr(self, addr):
-	self.getSelfAddr()
-	if addr <= self.elfEnd:
-	    return 1
-	else:
-	    return 0
+        self.getSelfAddr()
+        if addr <= self.elfEnd:
+            return 1
+        else:
+            return 0
