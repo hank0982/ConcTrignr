1c2706b9db2be0f282828290c43d3b3fc5a1b789
Neil Zhao <zzrcxb@gmail.com>
2017-07-11 08:21:21 +0000

    run simple test

--- code_coverage.py
+++ code_coverage.py
@@ -1,75 +1,77 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 ##
-##  Triton tool to perform code coverage
-##  Romain Thomas - 2015-09-26
+# Triton tool to perform code coverage
+# Romain Thomas - 2015-09-26
 ##
-## Description:
-## ------------
+# Description:
+# ------------
 ##
-## This tool aims to reach all basic blocks in a program using dynamic symbolic
-## resolution and the snapshot engine. The algorithm is based on Microsoft SAGE's
-## fuzzer.
+# This tool aims to reach all basic blocks in a program using dynamic symbolic
+# resolution and the snapshot engine. The algorithm is based on Microsoft SAGE's
+# fuzzer.
 ##
 ##
-## Output:
-## -------
+# Output:
+# -------
 ##
-## $ ./triton ./src/tools/code_coverage.py ./src/samples/code_coverage/test_atoi a
-## [+] Take Snapshot
-## [+] In main
-## [+] In main() we set :
-##         [0x7ffc92bdc54a] = 61 a
-##         [0x7ffc92bdc54b] = 61 a
-##         [0x7ffc92bdc54c] = 61 a
-## [+] Exit point
+# $ ./triton ./src/tools/code_coverage.py ./src/samples/code_coverage/test_atoi a
+# [+] Take Snapshot
+# [+] In main
+# [+] In main() we set :
+# [0x7ffc92bdc54a] = 61 a
+# [0x7ffc92bdc54b] = 61 a
+# [0x7ffc92bdc54c] = 61 a
+# [+] Exit point
 ## {140722770396490: 0}
 ## {140722770396490: 32}
 ## {140722770396490: 57}
-## [+] Restore snapshot
-## [+] In main
-## [+] In main() we set :
-##         [0x7ffc92bdc54a] = 39 9
-##         [0x7ffc92bdc54b] = 61 a
-##         [0x7ffc92bdc54c] = 61 a
-## [+] Exit point
+# [+] Restore snapshot
+# [+] In main
+# [+] In main() we set :
+# [0x7ffc92bdc54a] = 39 9
+# [0x7ffc92bdc54b] = 61 a
+# [0x7ffc92bdc54c] = 61 a
+# [+] Exit point
 ## {140722770396490: 57, 140722770396491: 0}
 ## {140722770396490: 57, 140722770396491: 8}
 ## {140722770396490: 56, 140722770396491: 56}
-## [+] Restore snapshot
-## [+] In main
-## [+] In main() we set :
-##         [0x7ffc92bdc54a] = 38 8
-##         [0x7ffc92bdc54b] = 38 8
-##         [0x7ffc92bdc54c] = 61 a
-## [+] Exit point
+# [+] Restore snapshot
+# [+] In main
+# [+] In main() we set :
+# [0x7ffc92bdc54a] = 38 8
+# [0x7ffc92bdc54b] = 38 8
+# [0x7ffc92bdc54c] = 61 a
+# [+] Exit point
 ## {140722770396490: 56, 140722770396491: 56, 140722770396492: 0}
 ## {140722770396490: 57, 140722770396491: 57, 140722770396492: 8}
 ## {140722770396490: 57, 140722770396491: 57, 140722770396492: 56}
 ## {140722770396490: 51, 140722770396491: 51, 140722770396492: 63}
-## [+] Restore snapshot
-## [+] In main
-## [+] In main() we set :
-##         [0x7ffc92bdc54a] = 33 3
-##         [0x7ffc92bdc54b] = 33 3
-##         [0x7ffc92bdc54c] = 3f ?
-## ok
-## [+] Exit point
-## [+] Done !
-## $
+# [+] Restore snapshot
+# [+] In main
+# [+] In main() we set :
+# [0x7ffc92bdc54a] = 33 3
+# [0x7ffc92bdc54b] = 33 3
+# [0x7ffc92bdc54c] = 3f ?
+# ok
+# [+] Exit point
+# [+] Done !
+# $
 ##
 
-# import  smt2lib
 import ast
 
-from    triton      import *
-from    pintool     import *
-from    collections import OrderedDict
-from    copy        import deepcopy
+from triton import *
+from pintool import *
+from collections import OrderedDict
+from copy import deepcopy
+from termcolor import colored
 
 icnt = 0
 
+
 class ElfAddrs:
+
     def __init__(self, filename):
         self.binary = Elf(filename)
         self.syms = self.binary.getSymbolsTable()
@@ -131,22 +133,20 @@ class Input(object):
         self.__dataAddr[address] = value
 
 
-
 class TritonExecution:
 
-    program     = None
-    input       = None
-    worklist    = None
+    program = None
+    input = None
+    worklist = None
     inputTested = None
-    entryPoint  = 0
-    exitPoint   = 0
-    whitelist   = None
-    myPC        = None
+    entryPoint = 0
+    exitPoint = 0
+    whitelist = None
+    myPC = None
     AddrAfterEP = 0
 
     @staticmethod
     def cbefore(instruction):
-        # print 'In before'
         global icnt
         icnt += 1
 
@@ -154,24 +154,28 @@ class TritonExecution:
             TritonExecution.AddrAfterEP = instruction.getNextAddress()
 
         if instruction.getAddress() == TritonExecution.AddrAfterEP:
-            TritonExecution.myPC = []                                  # Reset the path constraint
-            TritonExecution.input = TritonExecution.worklist.pop()     # Take the first input
-            TritonExecution.inputTested.append(TritonExecution.input)  # Add this input to the tested input
+            # Reset the path constraint
+            TritonExecution.myPC = []
+            TritonExecution.input = TritonExecution.worklist.pop() # Take the first input
+            # Add this input to the tested input
+            TritonExecution.inputTested.append(TritonExecution.input)
             return
 
         if instruction.getAddress() == TritonExecution.entryPoint and not isSnapshotEnabled():
-            print "[+] Take Snapshot"
+            print colored('[+] Take Snapshot', 'white')
             takeSnapshot()
             return
 
-        if getRoutineName(instruction.getAddress()) in TritonExecution.whitelist:
-            print instruction, getRoutineName(instruction.getAddress()), instruction.isBranch()
+        rt_name = getRoutineName(instruction.getAddress())
+        if rt_name in TritonExecution.whitelist and instruction.isBranch() and instruction.getType() != OPCODE.JMP and instruction.getOperands()[0].getType() == OPERAND.IMM:
+            print colored(getRoutineName(instruction.getAddress()) + ': ' + str(instruction), 'blue')
+            # next address next from the current one
+            addr1 = instruction.getNextAddress()
+            # Address in the instruction condition (branch taken)
+            addr2 = instruction.getOperands()[0].getValue()
 
-        if getRoutineName(instruction.getAddress()) in TritonExecution.whitelist and instruction.isBranch() and instruction.getType() != OPCODE.JMP and instruction.getOperands()[0].getType() == OPERAND.IMM:
-            addr1 = instruction.getNextAddress()              # next address next from the current one
-            addr2 = instruction.getOperands()[0].getValue()   # Address in the instruction condition (branch taken)
-
-            ripId = getSymbolicRegisterId(REG.RIP)            # Get the reference of the RIP symbolic register
+            # Get the reference of the RIP symbolic register
+            ripId = getSymbolicRegisterId(REG.RIP)
 
             # [PC id, address taken, address not taken]
             if instruction.isConditionTaken():
@@ -182,25 +186,34 @@ class TritonExecution:
             return
 
         if instruction.getAddress() == TritonExecution.exitPoint:
-            print "[+] Exit point"
+            print colored('[+] Exit point', 'magenta')
+            print colored(str(TritonExecution.myPC), 'red')
+            print colored('Bound = %d' % TritonExecution.input.bound, 'yellow')
 
             # SAGE algorithm
             # http://research.microsoft.com/en-us/um/people/pg/public_psfiles/ndss2008.pdf
             for j in range(TritonExecution.input.bound, len(TritonExecution.myPC)):
                 expr = []
-                for i in range(0,j):
+                for i in range(0, j):
                     ripId = TritonExecution.myPC[i][0]
-                    symExp = getFullAst(getSymbolicExpressionFromId(ripId).getAst())
+                    symExp = getFullAst(
+                        getSymbolicExpressionFromId(ripId).getAst())
                     addr = TritonExecution.myPC[i][1]
-                    expr.append(ast.assert_(ast.equal(symExp, ast.bv(addr,  CPUSIZE.QWORD_BIT))))
+                    expr.append(ast.assert_(
+                        ast.equal(symExp, ast.bv(addr,  CPUSIZE.QWORD_BIT))))
 
                 ripId = TritonExecution.myPC[j][0]
-                symExp = getFullAst(getSymbolicExpressionFromId(ripId).getAst())
+                symExp = getFullAst(
+                    getSymbolicExpressionFromId(ripId).getAst())
                 addr = TritonExecution.myPC[j][2]
-                expr.append(ast.assert_(ast.equal(symExp, ast.bv(addr,  CPUSIZE.QWORD_BIT))))
+                expr.append(ast.assert_(
+                    ast.equal(symExp, ast.bv(addr,  CPUSIZE.QWORD_BIT))))
 
                 expr = ast.compound(expr)
                 model = getModel(expr)
+
+                print colored("model: " + repr(model), 'yellow')
+                print colored("expr: " + repr(expr), 'green')
 
                 if len(model) > 0:
                     newInput = deepcopy(TritonExecution.input)
@@ -208,8 +221,9 @@ class TritonExecution:
 
                     for k, v in model.items():
                         symVar = getSymbolicVariableFromId(k)
-                        newInput.addDataAddress(symVar.getKindValue(), v.getValue())
-                    print newInput.dataAddr
+                        newInput.addDataAddress(
+                            symVar.getKindValue(), v.getValue())
+                    print colored('New input: ' + str(newInput.dataAddr), 'red')
 
                     isPresent = False
 
@@ -220,60 +234,63 @@ class TritonExecution:
                     if not isPresent:
                         TritonExecution.worklist.append(newInput)
 
-            # If there is input to test in the worklist, we restore the snapshot
+            # If there is input to test in the worklist, we restore the
+            # Snapshot
             if len(TritonExecution.worklist) > 0 and isSnapshotEnabled():
-                print "[+] Restore snapshot"
+                print colored('[+] Restore snapshot', 'white')
                 restoreSnapshot()
             return
         return
 
-
     @staticmethod
     def fini():
-        print '[+] Done !'
+        print colored('[+] Done !', 'white')
         return
-
 
     @staticmethod
     def mainAnalysis(threadId):
 
-        print "[+] In main"
+        print colored('[+] In main', 'white')
         CPUSIZE.REG = 8
-        rdi = getCurrentRegisterValue(REG.RDI) # argc
-        rsi = getCurrentRegisterValue(REG.RSI) # argv
-        argv0_addr = getCurrentMemoryValue(getCurrentRegisterValue(REG.RSI), CPUSIZE.REG) # argv[0] pointer
-        argv1_addr = getCurrentMemoryValue(rsi + CPUSIZE.REG, CPUSIZE.REG)                # argv[1] pointer
+        rdi = getCurrentRegisterValue(REG.RDI)  # argc
+        rsi = getCurrentRegisterValue(REG.RSI)  # argv
+        argv0_addr = getCurrentMemoryValue(
+            getCurrentRegisterValue(REG.RSI), CPUSIZE.REG)  # argv[0] pointer
+        argv1_addr = getCurrentMemoryValue(
+            rsi + CPUSIZE.REG, CPUSIZE.REG)                # argv[1] pointer
 
-        print "[+] In main() we set :"
+        print colored('[+] In main() we set :', 'magenta')
         od = OrderedDict(sorted(TritonExecution.input.dataAddr.items()))
 
         for k, v in od.iteritems():
-            print "\t[0x%x] = %x %c" % (k, v, v)
+            print colored('OD items: \t[0x%x] = %x %c' % (k, v, v), 'cyan')
             setCurrentMemoryValue(MemoryAccess(k, CPUSIZE.BYTE), v)
-            convertMemoryToSymbolicVariable(MemoryAccess(k, CPUSIZE.BYTE), "addr_%d" % k)
+            convertMemoryToSymbolicVariable(
+                MemoryAccess(k, CPUSIZE.BYTE), 'addr_%d' % k)
 
         for idx, byte in enumerate(TritonExecution.input.data):
-            print idx, byte
-            if argv1_addr + idx not in TritonExecution.input.dataAddr: # Not overwrite the previous setting
-                print "\t[0x%x] = %x %c" % (argv1_addr + idx, ord(byte), ord(byte))
-                setCurrentMemoryValue(MemoryAccess(argv1_addr + idx, CPUSIZE.BYTE), ord(byte))
-                convertMemoryToSymbolicVariable(MemoryAccess(argv1_addr + idx, CPUSIZE.BYTE), "addr_%d" % idx)
-
+            if argv1_addr + idx not in TritonExecution.input.dataAddr:  # Not overwrite the previous setting
+                print colored('Input data \t[0x%x] = %x %c' % (argv1_addr + idx, ord(byte), ord(byte)), 'green')
+                setCurrentMemoryValue(MemoryAccess(
+                    argv1_addr + idx, CPUSIZE.BYTE), ord(byte))
+                convertMemoryToSymbolicVariable(MemoryAccess(
+                    argv1_addr + idx, CPUSIZE.BYTE), 'addr_%d' % idx)
 
     @staticmethod
-    def run(inputSeed, elfAddrs, whitelist = []):
-        TritonExecution.entryPoint  = elfAddrs.getSymAddr('_start', 0)
+    def run(inputSeed, elfAddrs, whitelist=[]):
+        TritonExecution.entryPoint = elfAddrs.getSymAddr('_start', 0)
         TritonExecution.exitPoint = elfAddrs.getSymAddr('main', 1)
 
-        print TritonExecution.exitPoint, TritonExecution.entryPoint
+        # print TritonExecution.exitPoint, TritonExecution.entryPoint
 
-        TritonExecution.worklist    = [Input(inputSeed)]
+        TritonExecution.worklist = [Input(inputSeed)]
         TritonExecution.inputTested = []
-        TritonExecution.whitelist   = whitelist
+        TritonExecution.whitelist = whitelist
 
-        print 'Before start analysis'
+        print colored('Before start analysis', 'yellow')
         startAnalysisFromAddress(TritonExecution.entryPoint)
-        insertCall(TritonExecution.mainAnalysis, INSERT_POINT.ROUTINE_ENTRY, 'main')
+        insertCall(TritonExecution.mainAnalysis,
+                   INSERT_POINT.ROUTINE_ENTRY, 'main')
         insertCall(TritonExecution.cbefore,      INSERT_POINT.BEFORE)
         insertCall(TritonExecution.fini,         INSERT_POINT.FINI)
 
@@ -281,19 +298,18 @@ class TritonExecution:
         # addCallback(TritonExecution.cbefore,      INSERT_POINT.BEFORE)
         # addCallback(TritonExecution.fini,         INSERT_POINT.FINI)
 
-        print 'Before run program'
-        runProgram()
+        print colored('Before run program', 'yellow')
+        print runProgram()
 
-        print 'Instruction counter: ', icnt
+        print colored('Instruction counter: %d' % icnt, 'yellow')
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     # Set architecture
     setArchitecture(ARCH.X86_64)
-    elfAddrs = ElfAddrs("programs/c.out")
-    #TritonExecution.run("aaa", 0x4004a0, 0x40065D, ["main", "myatoi"])           # ./triton ./src/tools/code_coverage.py ./src/samples/code_coverage/test_atoi a
-    #TritonExecution.run("bad !", 0x400480, 0x40061B, ["main", "check"])          # ./triton ./src/tools/code_coverage.py ./src/samples/crackmes/crackme_xor abc
-    TritonExecution.run("2", elfAddrs, ["main", "check"])       # ./triton ./src/tools/code_coverage.py ./src/samples/crackmes/crackme_regex_fsm a
-    #TritonExecution.run("aaaaaaaa", 0x400460, 0x402ECA, ["main", "checkinput"])  # ./triton ./src/tools/code_coverage.py ./src/samples/crackmes/crackme_regex_fsm_obfuscated a
 
+    # elfAddrs = ElfAddrs("programs/test_atoi")
+    # TritonExecution.run("aaa", elfAddrs, ["main", "check"])
+
+    elfAddrs = ElfAddrs("programs/b.out")
+    TritonExecution.run("1", elfAddrs, ["main", "check"])
